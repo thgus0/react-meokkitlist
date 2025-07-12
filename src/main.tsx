37177bf1c25a21b748069b983c5c81ca043717bf
thgus0 <sohyun10521052@gmail.com>
2025-07-12 18:24:42 +0900

    chore: Emotion 스타일 라이브러리 추가 및 Reset CSS 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import App from '@/App.tsx'
+import { GlobalStyles } from '@/GlobalStyles'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>
 )
